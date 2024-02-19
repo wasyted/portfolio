2a9bf1d5b9edeb0ae8fa360879a30f653c8e6402
Matías Ezequiel Wasyluk <87504408+wasyted@users.noreply.github.com>
2024-02-19 15:21:34 -0300

    fixed unescaped quote

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
     <div className="bg-black w-full py-4 flex justify-center items-center text-xs flex-col gap-4">
       <div>
         <p>
-          Props to the creators of the resources i've used in this site:
+          Props to the creators of the resources i&apos;ve used in this site:
         </p>
         <ul className="list-disc list-inside">
           <li><a href="https://www.flaticon.es/iconos-gratis/javascript" title="javascript iconos">Javascript iconos creados por Freepik - Flaticon</a></li>
