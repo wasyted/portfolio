b0050be77f02a83a0da30c5eb67c18985072272f
Matías Ezequiel Wasyluk <87504408+wasyted@users.noreply.github.com>
2024-09-13 09:41:54 -0300

    Updated resumes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,28 +4,46 @@ const Resume = () => {
   return (
     <div className="max-w-4xl mx-auto py-4 px-4 sm:px-6 lg:px-8 bg-white">
       <header className="mb-4">
-        <div className='flex items-center justify-between gap-4 '>  
+        <div className='flex items-center justify-between gap-4 mb-3'>
           <h1 className="text-3xl font-bold mb-2 text-black">Matías Wasyluk</h1>
-          <p className="text-gray-800 text-lg">Web Developer | Programmer</p>
+          <p className="text-gray-800 text-md">Fullstack Developer | Informatics Engineering student</p>
         </div>
-        <p className="text-gray-600 text-md">Email: matiasezequielwasyluk@gmail.com</p>
-        <p className="text-gray-600 text-md">Phone: +54 011 35628327</p>
-        <p className="text-gray-600 text-md">Website: matiaswasyluk.vercel.app</p>
-        <p className="text-gray-600 text-md">Location: Buenos Aires, Argentina</p>
+        <p className="text-gray-600 text-md">Buenos Aires, Argentina</p>
+        <p className="text-gray-600 text-md">matiasezequielwasyluk@gmail.com</p>
+        <p className="text-gray-600 text-md">+54 011 35628327</p>
+        <p className="text-gray-600 text-md">Portfolio: matiaswasyluk.vercel.app</p>
+        <p className="text-gray-600 text-md">Linkedin: <a href='https://www.linkedin.com/in/matiaswasyluk/'>linkedin.com/in/matiaswasyluk</a></p>
       </header>
       <hr />
       <section className='mb-3'>
-        <h2 className="text-xl font-semibold mt-3 text-end text-black">Skills</h2>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black mb-3">Perfil profesional</h2>
+          <p className="text-gray-600">Informatics engineering student with a solid base in web developement and experience on self-managed projects. General understanding of programming and informatics concepts, with focus on good practices and interface design.<br /> Looking to expand knowledge in the web dev. area, applying effective communication, teamwork and problem-solving capabilites.</p>
+      </section>
+      <hr />
+      <section className='mb-3'>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black mb-3">Habilidades</h2>
         <ul className="list-disc list-inside text-sm">
-          <li className="text-gray-600">Proficient in HTML5, CSS3, and JavaScript (ES6+)</li>
-          <li className="text-gray-600">Experienced with React.js, Next.js, and Tailwind CSS</li>
-          <li className="text-gray-600">Skilled in Node.js, Express.js, and RESTful APIs</li>
-          <li className="text-gray-600">Familiar with MongoDB, MySQL, and database management</li>
-          <li className="text-gray-600">Knowledgeable in Git, GitHub, and version control</li>
-          <li className="text-gray-600">Strong understanding of responsive web design and UI/UX principles</li>
-          <li className="text-gray-600">Familiarized with object-oriented programming and C#</li>
-          <li className="text-gray-600">Experienced in deploying web applications using various platforms</li>
+          <li className="text-gray-600 mb-1">Soft skills: self-starter, proactive, team worker, communicative y receptive to learning and sharing good working practices.</li>
+          <li className="text-gray-600 mb-1">Basis at QA: Software testing and debugging concepts, quality assurance methods.</li>
+          <li className="text-gray-600 mb-1">Proficient in HTML5, CSS3 and JavaScript (ES6+)</li>
+          <li className="text-gray-600 mb-1">Frontend experience with React.js, Next.js and Tailwind CSS</li>
+          <li className="text-gray-600 mb-1">Backend knowledge with Node.js, Express.js and APIs RESTful</li>
+          <li className="text-gray-600 mb-1">Familiarized with MongoDB, MySQL and database management.</li>
+          <li className="text-gray-600 mb-1">Using Git, GitHub and version control.</li>
+          <li className="text-gray-600 mb-1">Solid basis in web design and UI/UX principles.</li>
         </ul>
+      </section>
+      <hr />
+      <section className='mb-3'>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black">Education</h2>
+        <div className='text-sm'>
+          <p className="font-semibold text-gray-800">Ingeniería en informática</p>
+          <p className="text-gray-600">Expected graduation: 2027 | Universidad Arturo Jauretche</p>
+        </div>
+        <div className='mt-3 text-sm'>
+          <p className="font-semibold text-gray-800">Fullstack Web Developement Certification</p>
+          <p className="text-gray-600">Date of certification: december 2023 | The Odin Project</p>
+        </div>
       </section>
       <hr />
       <section className='mb-3'>
@@ -41,45 +59,27 @@ const Resume = () => {
       </section>
       <hr />
       <section className='mb-3'>
-        <h2 className="text-xl font-semibold mt-3 text-end text-black">Education</h2>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black">Work experience</h2>
         <div className='text-sm'>
-          <p className="font-semibold text-gray-800">Ingeniería en informática</p>
-          <p className="text-gray-600">Expected Graduation: 2027 | Universidad Arturo Jauretche</p>
-        </div>
-        <div className='text-sm mt-3'>
-          <p className="font-semibold text-gray-800">Fullstack Web Developer Certification</p>
-          <p className="text-gray-600">Completion Date: December 2023 | The Odin Project</p>
-        </div>
-      </section>
-      <hr />
-      <section className='mb-3'>
-        <h2 className="text-xl font-semibold mt-3 text-end text-black">Experience</h2>
-        <div className='text-sm'>
-          <p className="font-semibold text-gray-800">Freelance Web Developer</p>
+          <p className="font-semibold text-gray-800">Freelance web developement</p>
           <p className="text-gray-600">Since 2022</p>
-          <p className="text-gray-600">Successfully delivered web solutions for clients, including a landing page for <span><a href="http://instagram.com/somos_huryn">@somos_huryn</a></span> to showcase their services and an e-commerce site for <span><a href="http://instagram.com/lalo.amasa">@lalo.amasa</a></span>, enabling order handling via WhatsApp integration.</p>
+          <p className="text-gray-600">Successfully developed and implemented web solutions for several clients, including a landing page for a window tint company, an ecommerce for a homemade bakery shop and a portfolio website for a filmmaker.</p>
         </div>
       </section>
       <hr />
       <section className='mb-3'>
-        <h2 className="text-xl font-semibold text-black text-end mt-3">Projects</h2>
-        <div className='text-sm mt-3'>
-          <p className="font-semibold text-gray-800">HurynWeb <span className='text-gray-500'>- Landing page</span></p>
-          <p className="text-gray-600">Designed and developed a professional landing page for <span><a href="http://instagram.com/somos_huryn">@somos_huryn</a></span> using HTML, CSS, and JavaScript, deployed on GitHub Pages.</p>
-          <p className="text-gray-600">GitHub Repository: <a href="https://github.com/wasyted/hurynWeb">https://github.com/wasyted/hurynWeb</a></p>
-          <p className="text-gray-600">Live Demo: <a href="https://wasyted.github.io/hurynWeb">https://wasyted.github.io/hurynWeb</a></p>
+        <h2 className="text-xl font-semibold text-black text-end mt-3">Proyectos</h2>
+        <div className='mt-3 text-sm'>
+          <p className="font-semibold text-gray-800">HurynWeb <span className='text-gray-500'>- Landing Page</span></p>
+          <p className="text-gray-600">Designed and developed a landing-page for <span><a href="http://instagram.com/somos_huryn">@somos_huryn</a></span> using exclusively vanilla HTML, CSS and JavaScript, implemented and hosted with GitHub Pages.</p>
+          <p className="text-gray-600">Repo: <a href="https://github.com/wasyted/hurynWeb">https://github.com/wasyted/hurynWeb</a></p>
+          <p className="text-gray-600">Live demo: <a href="https://wasyted.github.io/hurynWeb">https://wasyted.github.io/hurynWeb</a></p>
         </div>
-        <div className='text-sm mt-3'>
-          <p className="font-semibold text-gray-800">laloamasa <span className='text-gray-500'>- e-commerce</span></p>
-          <p className="text-gray-600">Developed an e-commerce platform for <span><a href="http://instagram.com/lalo.amasa">@lalo.amasa</a></span> using React, SASS, and fetchAPI, enabling seamless order processing via WhatsApp, deployed on Google Firebase.</p>
-          <p className="text-gray-600">GitHub Repository: <a href="https://github.com/wasyted/laloamasa">https://github.com/wasyted/laloamasa</a></p>
-          <p className="text-gray-600">Live Demo: <a href="https://laloamasa.web.app">https://laloamasa.web.app</a></p>
-        </div>
-        <div className='text-sm mt-3'>
-          <p className="font-semibold text-gray-800">noteblndr <span className='text-gray-500'>- Social Network</span></p>
-          <p className="text-gray-600">Built a full-stack social network prototype featuring user and post interactions using ExpressJS, MongoDB, EJS, and Tailwind CSS, deployed on Railway.</p>
-          <p className="text-gray-600">GitHub Repository: <a href="https://github.com/wasyted/auth-test-app">https://github.com/wasyted/auth-test-app</a></p>
-          <p className="text-gray-600">Live Demo: <a href="https://noteblndr.up.railway.app">https://noteblndr.up.railway.app</a></p>
+        <div className='mt-3 text-sm'>
+          <p className="font-semibold text-gray-800">noteblndr <span className='text-gray-500'>- Social network</span></p>
+          <p className="text-gray-600">Full-stack social network prototype with user interaction through posts using ExpressJS, MongoDB, EJS y Tailwind CSS, implemented at Railway hosting.</p>
+          <p className="text-gray-600">Repo: <a href="https://github.com/wasyted/auth-test-app">https://github.com/wasyted/auth-test-app</a></p>
+          <p className="text-gray-600">Live demo: <a href="https://noteblndr.up.railway.app">https://noteblndr.up.railway.app</a></p>
         </div>
       </section>
     </div>
--- src/components/ResumeSpanish.jsx
+++ src/components/ResumeSpanish.jsx
@@ -4,27 +4,33 @@ const ResumeSpanish = () => {
   return (
     <div className="max-w-4xl mx-auto py-4 px-4 sm:px-6 lg:px-8 bg-white">
       <header className="mb-4">
-        <div className='flex items-center justify-between gap-4'>
+        <div className='flex items-center justify-between gap-4 mb-3'>
           <h1 className="text-3xl font-bold mb-2 text-black">Matías Wasyluk</h1>
-          <p className="text-gray-800 text-md">Desarrollador Web | Programador</p>
+          <p className="text-gray-800 text-md">Desarrollador Web | Estudiante de Ingeniería Informática</p>
         </div>
-        <p className="text-gray-600 text-md">Email: matiasezequielwasyluk@gmail.com</p>
-        <p className="text-gray-600 text-md">Teléfono: +54 011 35628327</p>
+        <p className="text-gray-600 text-md">Buenos Aires, Argentina</p>
+        <p className="text-gray-600 text-md">matiasezequielwasyluk@gmail.com</p>
+        <p className="text-gray-600 text-md">+54 011 35628327</p>
         <p className="text-gray-600 text-md">Sitio web: matiaswasyluk.vercel.app</p>
-        <p className="text-gray-600 text-md">Ubicación: Buenos Aires, Argentina</p>
+        <p className="text-gray-600 text-md">Linkedin: <a href='https://www.linkedin.com/in/matiaswasyluk/'>linkedin.com/in/matiaswasyluk</a></p>
       </header>
       <hr />
       <section className='mb-3'>
-        <h2 className="text-xl font-semibold mt-3 text-end text-black">Habilidades</h2>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black mb-3">Perfil profesional</h2>
+          <p className="text-gray-600">Estudiante de Ingeniería en Informática con una sólida base en desarrollo web y experiencia en proyectos de programación. Posee conocimientos generales de informática y programación, con un enfoque en buenas practicas de programación y diseño de interfaces.<br /> Busca expandir su experiencia en el área de desarrollo web, aplicando habilidades de comunicación efectiva, trabajo en equipo y capacidad de análisis y resolución de problematicas.</p>
+      </section>
+      <hr />
+      <section className='mb-3'>
+        <h2 className="text-xl font-semibold mt-3 text-end text-black mb-3">Habilidades</h2>
         <ul className="list-disc list-inside text-sm">
-          <li className="text-gray-600">Experto en HTML5, CSS3 y JavaScript (ES6+)</li>
-          <li className="text-gray-600">Experiencia con React.js, Next.js y Tailwind CSS</li>
-          <li className="text-gray-600">Conocimientos en Node.js, Express.js y APIs RESTful</li>
-          <li className="text-gray-600">Familiarizado con MongoDB, MySQL y gestión de bases de datos</li>
-          <li className="text-gray-600">Conocimientos de Git, GitHub y control de versiones</li>
-          <li className="text-gray-600">Entendimiento sólido de diseño web receptivo y principios de UI/UX</li>
-          <li className="text-gray-600">Familiarizado con programación orientada a objetos y C#</li>
-          <li className="text-gray-600">Experiencia en implementar aplicaciones web usando diversas plataformas</li>
+          <li className="text-gray-600 mb-1">Habilidades blandas: self-starter, proactividad, trabajo en equipo, predispuesto, comunicativo y receptivo a aprender.</li>
+          <li className="text-gray-600 mb-1">Conocimientos básicos de QA: concepto de pruebas de software, debugging y metodologías de calidad</li>
+          <li className="text-gray-600 mb-1">Proficiente en HTML5, CSS3 y JavaScript (ES6+)</li>
+          <li className="text-gray-600 mb-1">Experiencia Frontend con React.js, Next.js y Tailwind CSS</li>
+          <li className="text-gray-600 mb-1">Conocimientos Backend en Node.js, Express.js y APIs RESTful</li>
+          <li className="text-gray-600 mb-1">Familiarizado con MongoDB, MySQL y gestión de bases de datos</li>
+          <li className="text-gray-600 mb-1">Conocimientos de Git, GitHub y control de versiones</li>
+          <li className="text-gray-600 mb-1">Entendimiento sólido de diseño web receptivo y principios de UI/UX</li>
         </ul>
       </section>
       <hr />
@@ -57,7 +63,7 @@ const ResumeSpanish = () => {
         <div className='text-sm'>
           <p className="font-semibold text-gray-800">Desarrollador Web Freelance</p>
           <p className="text-gray-600">Desde 2022</p>
-          <p className="text-gray-600">Entregué exitosamente soluciones web para clientes, incluyendo una página de aterrizaje para @Huryn para mostrar sus servicios y un sitio de comercio electrónico para @laloamasa, permitiendo el procesamiento de pedidos a través de WhatsApp.</p>
+          <p className="text-gray-600">Entregué exitosamente soluciones web para clientes, incluyendo una landing-page para @Huryn la cual expone sus servicios ademas de un e-commerce para @laloamasa, permitiendo el procesamiento de pedidos a través de WhatsApp.</p>
         </div>
       </section>
       <hr />
@@ -68,12 +74,6 @@ const ResumeSpanish = () => {
           <p className="text-gray-600">Diseñé y desarrollé una landing page profesional para <span><a href="http://instagram.com/somos_huryn">@somos_huryn</a></span> utilizando HTML, CSS y JavaScript, implementado en GitHub Pages.</p>
           <p className="text-gray-600">Repositorio en GitHub: <a href="https://github.com/wasyted/hurynWeb">https://github.com/wasyted/hurynWeb</a></p>
           <p className="text-gray-600">Demo en vivo: <a href="https://wasyted.github.io/hurynWeb">https://wasyted.github.io/hurynWeb</a></p>
-        </div>
-        <div className='mt-3 text-sm'>
-          <p className="font-semibold text-gray-800">laloamasa <span className='text-gray-500'>- e-commerce</span></p>
-          <p className="text-gray-600">Desarrollé una plataforma de comercio electrónico para <span><a href="http://instagram.com/lalo.amasa">@lalo.amasa</a></span> utilizando React, SASS y fetchAPI, permitiendo un procesamiento de pedidos a través de WhatsApp, implementado en Google Firebase.</p>
-          <p className="text-gray-600">Repositorio en GitHub: <a href="https://github.com/wasyted/laloamasa">https://github.com/wasyted/laloamasa</a></p>
-          <p className="text-gray-600">Demo en vivo: <a href="https://laloamasa.web.app">https://laloamasa.web.app</a></p>
         </div>
         <div className='mt-3 text-sm'>
           <p className="font-semibold text-gray-800">noteblndr <span className='text-gray-500'>- Red social</span></p>
