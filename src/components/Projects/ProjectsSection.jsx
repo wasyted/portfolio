17cb0b69de67b427a46ff5b9500be80d1abcfb82
Matías Ezequiel Wasyluk <87504408+wasyted@users.noreply.github.com>
2025-05-14 11:02:22 -0300

    removed comments

--- src/components/Projects/ProjectsSection.jsx
+++ src/components/Projects/ProjectsSection.jsx
@@ -138,58 +138,6 @@ export default function Projects() {
           <a target="_blank" href="https://wasyted.github.io/hurynWeb/" className="hover:bg-[#b6f2c0] hover:aspect-square hover:text-[#f3f9f4] transition-all bg-[#f3f9f4] text-[#0d0d0d] flex items-center text-center justify-center aspect-video p-2 w-12">▶</a>
         </motion.div>
       </motion.div>
-      {/*<div className="sm:mt-24 text-center p-4 sm:py-10 bg-gradient-to-br from-blue-700 to-blue-600 rounded-lg">
-        <h1 className="font-bold text-4xl h-max bg-gradient-to-br text-transparent bg-clip-text from-black to-slate-600 dark:from-white dark:to-zinc-300">
-          Tools and technologies
-        </h1>
-        <div className="flex items-center justify-center flex-wrap mt-5 gap-2 sm:gap-4 max-w-2xl mx-auto grayscale">
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="css3" width={40} height={40} src="/logos/css-3.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="tailwindcss" width={40} height={40} src="/logos/Tailwind_CSS_Logo.svg.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="html" width={40} height={40} src="/logos/html-5.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="js" width={40} height={40} src="/logos/js.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="typescript" width={40} height={40} src="/logos/Typescript_logo_2020.svg.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="webpack" width={40} height={40} src="/logos/webpack.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="react" width={40} height={40} src="/logos/React-icon.svg.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="nextjs" width={40} height={40} src="/logos/next-js-logo-freelogovectors.net_.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="expressjs" className="dark:invert" width={40} height={40} src="/logos/Expressjs.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="ejs" width={40} height={40} src="/logos/ejs.svg"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="mongodb" width={40} height={40} src="/logos/MongoDB_Logo.svg.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="github" width={40} height={40} src="/logos/github-mark.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="railway" width={40} height={40} src="/logos/logo-dark.png"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="vercel" width={40} height={40} src="/logos/vercel.svg"></Image>
-          </div>
-          <div className="overflow-hidden aspect-square p-8 flex items-center justify-center bg-white/25 rounded-lg hover:scale-110 backdrop-blur-lg">
-            <Image priority alt="firebase" width={40} height={40} src="/logos/logo-logomark.png"></Image>
-          </div>
-        </div>
-      </div>*/}
     </div>
   )
 }
